0178d8a51c35b8613d6fa1460db1d38d87ea027e
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-10-14 20:38:14 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      TRUE
+    else
+      FALSE
+    end
   end
 
   def question?
-
+ if self.end_with?("?")
+      TRUE
+    else
+      FALSE
+    end
   end
 
   def exclamation?
-
+ if self.end_with?("!")
+      TRUE
+    else
+      FALSE
+    end
   end
 
   def count_sentences
-
+ new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+   new_array.size
   end
 end

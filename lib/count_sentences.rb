b08cf9c13b5d3091172c0e3b224a994ed65c44ce
Rokhaya  Ndiaye <rokhayamuslima1@gmail.com>
2019-10-19 17:17:49 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,32 @@
 require 'pry'
 
 class String
-
-  def sentence?
-    
-  end
+end
+  def sentence?(greeting)
+      self.greeting =="HI!"
+      return self.greeting
+  end 
+   if "HI!".end_with?("!")
+     true
+   else
+     false
+   end
+   
 
   def question?
 
   end
 
   def exclamation?
-
+    "exclamation?" = question_mark
+   if exclamation?.end_with("?") 
+    true
+  else
+    false
   end
 
-  def count_sentences
+  def count_sentences ("count_sentences")
+    self.count_sentences = "This is a string! It has three sentences. Right?"
 
   end
 end

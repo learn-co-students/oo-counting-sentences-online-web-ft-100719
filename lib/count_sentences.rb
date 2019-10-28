1cc2bd39c52020e460738146b9707b70bc8593f5
Michael Calhoun <iengineersoftware@gmail.com>
2019-10-28 12:29:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    separated = self.split(/[.|?|!]/)
+    selected = separated.select {|i| i != ""}
+    count = selected.length
   end
 end

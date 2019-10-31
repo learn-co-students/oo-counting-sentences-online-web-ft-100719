837698e2084e10321882a7a733e379004ad04ac6
Hans Foster <fosterhans@gmail.com>
2019-10-31 07:10:09 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true 
+    else
+      false 
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true 
+    else
+      false 
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true 
+    else
+      false 
+    end
   end
 
   def count_sentences
-
+    new_sentences = []
+    if self.include?(".")
+      new_sentences = self.split(/[.!?]/)
+    end
+    new_sentences2 = []
+    new_sentences.each do |value|
+      if value != "" && value != "?"
+        new_sentences2 << value
+      end
+    end
+    new_sentences2.size
   end
 end

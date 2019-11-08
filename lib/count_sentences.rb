41d866ed8fbad9c1ea2488428699de9eb2320916
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-08 22:47:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,40 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with? (".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    sentence_array=self.split(".")
+    sentence_array.each do |word|
+      if word.include?("!")
+        word.split("!").each do |str|
+          if str !=""
+            sentence_array<<str
+          end
+        end
+      end
+    end
+    sentence_array.length
   end
 end
